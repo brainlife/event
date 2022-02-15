1d30e458e90ccc4fc711e57fab1d48b103755bd4
Dheeraj Bhatia <dheerajb@buffalo.edu>
2022-02-15 16:44:22 -0500

    jwt generate script

--- api/config/gen_jwt.sh
+++ api/config/gen_jwt.sh
@@ -1,3 +1,4 @@
 
 #used to access other sca services
 ~/git/auth/bin/auth.js issue --scopes '{ "sca": ["admin"] }' --sub 'event' --out event.jwt
+# ~/brainlife_setup/auth/bin/auth.js issue --scopes '{ "sca": ["admin"] }' --sub 'event' --out event.jwt
